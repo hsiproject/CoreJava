1c0aa7ff45d3b2e601ee4ffacd9e71c1459be1c5
ravip <ravip@DESKTOP-2D6F20L>
2022-11-30 11:23:40 +0530

    push

---
+++ Data Types/src/com/project/datatypes/DataTypes.java
@@ -0,0 +1,37 @@
+package com.project.datatypes;
+
+public class DataTypes {
+	public static void main(String[]args) {
+		byte byteValue=30;
+		short shortValue=150;
+		int intValue=1000;
+		long longValue=1234506789;
+		float floatValue=150.23f;
+		double doubleValue=140.6262;
+		char charValue='M';
+		boolean isValid=true;
+		String Name="Manasa";
+		String symbol="$**&^%$#@";
+		String numbers="1332778";
+		String decimalnumber= "123.878";
+		String alphanumeric="mw659ver";
+		String bl="True";
+		System.out.println("The value of Byte is: "+byteValue);
+		System.out.println("The value of short is:"+shortValue);
+		System.out.println("The value of Int is:"+intValue);
+		System.out.println("The value of Long is:"+longValue);
+		System.out.println("The value of Float is:"+floatValue);
+		System.out.println("The value of Double is:"+doubleValue);
+		System.out.println("The value of Char is:"+charValue);
+		System.out.println("The value of Boolean is:"+isValid);
+		System.out.println("The value of Name is:"+Name);
+		System.out.println("The value of Symbol is:"+symbol);
+		System.out.println("The value of Numbers is:"+numbers);
+		System.out.println("The value of decimal is:"+decimalnumber);
+		System.out.println("The value of alpha is:"+alphanumeric);
+		System.out.println("The value of boolean is:"+bl);
+		
+
+}
+	
+}
