c0e1a7427f0f199cc914a76b31e974105b9c30f9
ravip <ravip@DESKTOP-2D6F20L>
2022-12-01 11:26:09 +0530

    To Branch

---
+++ First Programme/src/com/project/demo/FirstProgramme.java
@@ -0,0 +1,12 @@
+package com.project.demo;
+
+public class FirstProgramme {
+	public static void main(String[]args) {
+		System.out.println("Hello World");
+		System.out.println("Welcome to Java sessions");//Print Command
+		System.out.println("Welcome to Salesforce Internship");
+     
+   }
+   
+
+}
